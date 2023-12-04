7d25b9b449c06fc97ec79a014aa06b6267e25b03
Jakob K <dev@jakobk.de>
2023-12-04 06:54:55 +0100

    Solve day 4

---
+++ src/kotlin/Day04.kt
@@ -0,0 +1,28 @@
+import kotlin.math.pow
+
+fun main() = day(4) {
+
+    val matches = inputLines
+        .map { it.split(":").last().trim() }
+        .map { line ->
+            val lists = line.split("|")
+                .map { s -> s.trim().split(Regex("""\s+""")).map { it.toInt() } }
+            lists.first() to lists.last()
+        }
+        .map { pair -> pair.second.count { it in pair.first } }
+
+    part1 {
+        matches.sumOf { 2.0.pow(it - 1).toLong() }
+    }
+
+    part2 {
+        val scratched = HashMap<Int, Long>()
+        fun checkCard(index: Int): Long =
+            scratched.getOrPut(index)
+            { ((index + 1)..(index + matches[index])).sumOf(::checkCard) + 1 }
+        matches.indices.sumOf(::checkCard)
+    }
+
+    expectPart1 = 13L
+    expectPart2 = 30L
+}
