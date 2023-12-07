1142da41563df6a25e38f7f97edaae2d4b01a7ae
Jakob K <dev@jakobk.de>
2023-12-07 07:13:33 +0100

    Shorten day 6

--- src/kotlin/Day06.kt
+++ src/kotlin/Day06.kt
@@ -1,16 +1,14 @@
 fun main() = day(6) {
 
-    fun String.lineInts() =
-        if (currentPart == 1)
-            split(Regex("""\s+""")).drop(1).map { it.toLong() }
-        else
-            listOf(split(Regex("""\s+""")).drop(1).joinToString("").toLong())
+    fun String.lineInts() = when (currentPart) {
+        1 -> split(Regex("""\s+""")).drop(1).map { it.toLong() }
+        else -> listOf(split(Regex("""\s+""")).drop(1).joinToString("").toLong())
+    }
 
     bothParts {
         val times = inputLines.first().lineInts()
-        val races = inputLines.last().lineInts().mapIndexed { index, distance -> times[index] to distance }
-
-        races
+        inputLines.last().lineInts()
+            .mapIndexed { index, distance -> times[index] to distance }
             .map { (t, d) -> (0..t).count { (t - it) * it > d } }
             .reduce { acc, i -> acc * i }
     }
